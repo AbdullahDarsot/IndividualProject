f91fb5d555c3a326da8969569b23205ca62e8f21
Abdullah Darsot <abdullahdarsot1@gmail.com>
2021-11-01 14:44:01 +0000

    Commit after 3 class created

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 //first commit
+
+class Fish {
+    constructor(name, strength, speed) {
+        this.name = name;
+        this.strength = strength;
+        this.speed = speed;
+        this.tail = true;
+    }
+}
+
+const Tunna = new Fish ('Tunna', 7, 9)
+const Shark = new Fish ('Shark', 10, 8)
+
+class Ocean {
+    constructor(name, color, FishLevel) {
+        this.name = name;
+        this.color = color;
+        this.FishLevel = FishLevel;
+    }
+}
+
+const Pacific = new Ocean('Pacific', 'Blue', 8)
+const Antartic = new Ocean('Antartic', 'Blue / Grey', 6)
+
+class FisherMan {
+    constructor(name, age, gender, strength) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.strength = strength;
+    }
+}
+
+const Mark = new FisherMan('Mark', 35, 'Male', 7)
+const Jenny = new FisherMan('Jenny', 25, 'Female', 7)
+
+console.log()
